e32fc31305f69f5edbbd612c1e19a50ce47fd4f0
Samour <aaron.burke.cpp@gmail.com>
2021-10-16 03:36:51 +1100

    Add React

---
+++ renderer/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => (
+  <div>
+    <h1>This is my react app!</h1>
+    <Counter/>
+  </div>
+);
+
+export default App;
---
+++ renderer/src/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div>
+      <p>The current count is: {count}</p>
+      <div className='button-row'>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
